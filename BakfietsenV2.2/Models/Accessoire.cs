d9c1bdabd09b3b14292aaa1b37ea3d9bd2be31c7
yoomster <nperenboom@hotmail.com>
2024-07-01 14:36:03 +0200

    update to models

---
+++ BakfietsenV2.2/Models/Accessoire.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakfietsenV2._2.Models
+{
+    public class Accessoire
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
