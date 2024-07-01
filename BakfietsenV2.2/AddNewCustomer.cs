9322dfc485419536900467c8d29354b15c574b90
yoomster <nperenboom@hotmail.com>
2024-07-01 14:36:26 +0200

    Add form for adding new customer to db

---
+++ BakfietsenV2.2/AddNewCustomer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BakfietsenV2._2
+{
+    public partial class AddNewCustomer : Form
+    {
+        public AddNewCustomer()
+        {
+            InitializeComponent();
+        }
+    }
+}
