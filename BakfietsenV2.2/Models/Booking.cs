f052e3ba19a69e26adbed7e4d3e6cd89e0d66a97
yoomster <nperenboom@hotmail.com>
2024-07-01 13:28:27 +0200

    Add project files.

---
+++ BakfietsenV2.2/Models/Booking.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakfietsenV2._2.Models
+{
+    internal class Booking
+    {
+    }
+}
